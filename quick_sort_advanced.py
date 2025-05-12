861437ff7beb20f1b129d1383ee9cc1cd7d30511
Winnie <b323107006@tmu.edu.tw>
2025-05-12 14:35:28 +0800

    update

--- quick_sort_advanced.py
+++ quick_sort_advanced.py
@@ -50,13 +50,11 @@ if __name__ == "__main__":
         if user_input.strip() == "":
             data = default_data.copy()
         else:
-            # 嘗試轉換為整數列表，並檢查格式錯誤
             str_items = [item.strip() for item in user_input.split(',')]
             if "" in str_items:
                 raise ValueError("有空白值或重複逗號")
             data = [int(item) for item in str_items]
 
-        # 升／降序選擇
         order_input = input("請選擇排序方式：輸入A表示升序，輸入D表示降序：\n").strip().upper()
         if order_input == "D":
             ascending = False
@@ -66,11 +64,9 @@ if __name__ == "__main__":
             raise ValueError("只能輸入A(升序)或D(降序)")
 
         print(f"原始陣列：{data}\n")
-
         quick_sort(data, 0, len(data) - 1)
-
         print(f"排序後陣列：{data}")
         print(f"總交換次數：{swap_count}\n")
 
-    except ValueError as VE:
-        print(f"輸入錯誤：{VE}")
+    except ValueError:
+        print(f"輸入錯誤：{ValueError}")
